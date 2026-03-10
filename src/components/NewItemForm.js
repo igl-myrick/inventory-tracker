fa56a3e85729b9e56bbcf08e115dadf06282f158
unknown <igl.myrick@gmail.com>
2026-03-10 15:03:40 -0700

    add NewItemForm component

---
+++ src/components/NewItemForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { v4 } from "uuid";
+
+function NewItemForm(props) {
+  function handleNewItemFormSubmission(event) {
+    event.preventDefault();
+    props.onNewItemCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      roast: event.target.roast.value,
+      id: v4()
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewItemFormSubmission}
+        buttonText="Submit"/>
+    </React.Fragment>
+  );
+}
+
+NewItemForm.propTypes = {
+  onNewItemCreation: PropTypes.func
+}
+
+export default NewItemForm;
