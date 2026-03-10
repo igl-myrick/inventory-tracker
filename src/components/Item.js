a87760ff5b99592a1b9a55f7283031b9c70627d2
unknown <igl.myrick@gmail.com>
2026-03-10 16:03:40 -0700

    add Item component

---
+++ src/components/Item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Item(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenItemClicked(props.id)}>
+        <h3>{props.name} - ${props.price}</h3>
+        <p>From {props.origin} - Roast: {props.roast}</p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Item.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.string,
+  roast: PropTypes.string,
+  whenItemClicked: PropTypes.func
+}
+
+export default Item;
