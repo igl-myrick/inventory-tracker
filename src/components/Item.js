7a476cca1f52eba0dcf19e32bcc7f2d519c87cef
unknown <igl.myrick@gmail.com>
2026-03-11 15:28:31 -0700

    add conditional rendering for stock

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,12 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Item(props) {
+  let stockElem = null;
+  if (props.stock > 0) {
+    stockElem = <p>Stock: {props.stock} pounds</p>;
+  } else {
+    stockElem = <p>Out of Stock</p>;
+  }
+
   return (
     <React.Fragment>
       <div onClick={() => props.whenItemClicked(props.id)}>
         <h3>{props.name} - ${props.price}</h3>
         <p>From {props.origin} - Roast: {props.roast}</p>
-        <p>Stock: {props.stock} pounds</p>
+        {stockElem} 
         <hr/>
       </div>
     </React.Fragment>
